970f17e0f7c0cae86e8da89612cab139fa273af2
Trumane12311 <Trumane12311@gmail.com>
2021-06-21 01:37:32 -0400

    Updated, only missing state abbreviations

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -1,17 +1,20 @@
 // @TODO: YOUR CODE HERE!
 // set the dimensions and margins of the graph
-let margin = {top: 10, right: 30, bottom: 30, left: 50},
+let margin = {top: 10, right: 30, bottom: 50, left: 50},
     width = 950 - margin.left - margin.right,
-    height = 750 - margin.top - margin.bottom;
+    height = 775 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-let svg = d3.select("#scatter")
+const svg = d3.select("#scatter")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
+
+let chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Read the data
 d3.csv("D3_data_journalism/data.csv").then(censusData => {
@@ -38,6 +41,13 @@ d3.csv("D3_data_journalism/data.csv").then(censusData => {
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
+  
+  let xlabel = svg.append("text")
+    .attr("x", 400)
+    .attr("y", 750)
+    .attr("value", "income") // value to grab for event listener
+    .classed("active", true)
+    .text("Median Household Income ($)");
 
   // Add Y axis
   let y = d3.scaleLinear()
@@ -89,352 +99,37 @@ d3.csv("D3_data_journalism/data.csv").then(censusData => {
   }
 
   // Add dots
-  svg.append('g')
+  let circlesGroup = svg.append('g')
     .selectAll("dot")
     .data(censusData.filter(function(d,i){return i})) // the .filter part is just to keep a few dots on the chart, not all of them
     .enter()
     .append("circle")
       .attr("cx", function (d) { return x(d.income); } )
       .attr("cy", function (d) { return y(d.healthcare); } )
-      .attr("r", 7)
+      .attr("r", 15)
       .style("fill", "#69b3a2")
-      .style("opacity", 0.3)
+      .style("opacity", 0.5)
       .style("stroke", "white")
     .on("mouseover", mouseover )
     .on("mousemove", mousemove )
     .on("mouseleave", mouseleave )
+  
+  let circleLabels = chartGroup.selectAll("text").data(censusData).enter().append("text");
+
+    circleLabels
+      .attr("x", function(d) { return d.income; })
+      .attr("y", function(d) { return d.healthcare; })
+      .text(function(d) { return d.abbr; })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "5px")
+      .attr("fill", "white");
     
     // Label the Y-Axis
     svg.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left)
-    .attr("x", 0 - (height / 2))
+    .attr("x", 50 - (height / 2))
     .attr("dy", "1em")
-    .classed("axis-text", true)
-    .text("Healthcare");
-    
-    // Label the X-Axis
-    svg.append("text")
-    .attr("transform", "rotate(0)")
-    .attr("y", 0 - (width / 2))
-    .attr("x", 0 - margin.bottom)
-    .attr("dx", "1em")
-    .classed("axis-text", true)
-    .text("Income");
-
-    d3.functor = function functor(v) {
-      return typeof v === "function" ? v : function() {
-        return v;
-      };
-    };
-    
-    d3.tip = function() {
-    
-      var direction = d3_tip_direction,
-          offset    = d3_tip_offset,
-          html      = d3_tip_html,
-          node      = initNode(),
-          svg       = null,
-          point     = null,
-          target    = null
-    
-      function tip(vis) {
-        svg = getSVGNode(vis)
-        point = svg.createSVGPoint()
-        document.body.appendChild(node)
-      }
-    
-      // Public - show the tooltip on the screen
-      //
-      // Returns a tip
-      tip.show = function() {
-        var args = Array.prototype.slice.call(arguments)
-        if(args[args.length - 1] instanceof SVGElement) target = args.pop()
-    
-        var content = html.apply(this, args),
-            poffset = offset.apply(this, args),
-            dir     = direction.apply(this, args),
-            nodel   = getNodeEl(),
-            i       = directions.length,
-            coords,
-            scrollTop  = document.documentElement.scrollTop || document.body.scrollTop,
-            scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft
-    
-        nodel.html(content)
-          .style('position', 'absolute')
-          .style('opacity', 1)
-          .style('pointer-events', 'all')
-    
-        while(i--) nodel.classed(directions[i], false)
-        coords = direction_callbacks[dir].apply(this)
-        nodel.classed(dir, true)
-          .style('top', (coords.top +  poffset[0]) + scrollTop + 'px')
-          .style('left', (coords.left + poffset[1]) + scrollLeft + 'px')
-    
-        return tip
-      }
-    
-      // Public - hide the tooltip
-      //
-      // Returns a tip
-      tip.hide = function() {
-        var nodel = getNodeEl()
-        nodel
-          .style('opacity', 0)
-          .style('pointer-events', 'none')
-        return tip
-      }
-    
-      // Public: Proxy attr calls to the d3 tip container.  Sets or gets attribute value.
-      //
-      // n - name of the attribute
-      // v - value of the attribute
-      //
-      // Returns tip or attribute value
-      tip.attr = function(n, v) {
-        if (arguments.length < 2 && typeof n === 'string') {
-          return getNodeEl().attr(n)
-        } else {
-          var args =  Array.prototype.slice.call(arguments)
-          d3.selection.prototype.attr.apply(getNodeEl(), args)
-        }
-    
-        return tip
-      }
-    
-      // Public: Proxy style calls to the d3 tip container.  Sets or gets a style value.
-      //
-      // n - name of the property
-      // v - value of the property
-      //
-      // Returns tip or style property value
-      tip.style = function(n, v) {
-        // debugger;
-        if (arguments.length < 2 && typeof n === 'string') {
-          return getNodeEl().style(n)
-        } else {
-          var args = Array.prototype.slice.call(arguments);
-          if (args.length === 1) {
-            var styles = args[0];
-            Object.keys(styles).forEach(function(key) {
-              return d3.selection.prototype.style.apply(getNodeEl(), [key, styles[key]]);
-            });
-          }
-        }
-    
-        return tip
-      }
-    
-      // Public: Set or get the direction of the tooltip
-      //
-      // v - One of n(north), s(south), e(east), or w(west), nw(northwest),
-      //     sw(southwest), ne(northeast) or se(southeast)
-      //
-      // Returns tip or direction
-      tip.direction = function(v) {
-        if (!arguments.length) return direction
-        direction = v == null ? v : d3.functor(v)
-    
-        return tip
-      }
-    
-      // Public: Sets or gets the offset of the tip
-      //
-      // v - Array of [x, y] offset
-      //
-      // Returns offset or
-      tip.offset = function(v) {
-        if (!arguments.length) return offset
-        offset = v == null ? v : d3.functor(v)
-    
-        return tip
-      }
-    
-      // Public: sets or gets the html value of the tooltip
-      //
-      // v - String value of the tip
-      //
-      // Returns html value or tip
-      tip.html = function(v) {
-        if (!arguments.length) return html
-        html = v == null ? v : d3.functor(v)
-    
-        return tip
-      }
-    
-      // Public: destroys the tooltip and removes it from the DOM
-      //
-      // Returns a tip
-      tip.destroy = function() {
-        if(node) {
-          getNodeEl().remove();
-          node = null;
-        }
-        return tip;
-      }
-    
-      function d3_tip_direction() { return 'n' }
-      function d3_tip_offset() { return [0, 0] }
-      function d3_tip_html() { return ' ' }
-    
-      var direction_callbacks = {
-        n:  direction_n,
-        s:  direction_s,
-        e:  direction_e,
-        w:  direction_w,
-        nw: direction_nw,
-        ne: direction_ne,
-        sw: direction_sw,
-        se: direction_se
-      };
-    
-      var directions = Object.keys(direction_callbacks);
-    
-      function direction_n() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.n.y - node.offsetHeight,
-          left: bbox.n.x - node.offsetWidth / 2
-        }
-      }
-    
-      function direction_s() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.s.y,
-          left: bbox.s.x - node.offsetWidth / 2
-        }
-      }
-    
-      function direction_e() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.e.y - node.offsetHeight / 2,
-          left: bbox.e.x
-        }
-      }
-    
-      function direction_w() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.w.y - node.offsetHeight / 2,
-          left: bbox.w.x - node.offsetWidth
-        }
-      }
-    
-      function direction_nw() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.nw.y - node.offsetHeight,
-          left: bbox.nw.x - node.offsetWidth
-        }
-      }
-    
-      function direction_ne() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.ne.y - node.offsetHeight,
-          left: bbox.ne.x
-        }
-      }
-    
-      function direction_sw() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.sw.y,
-          left: bbox.sw.x - node.offsetWidth
-        }
-      }
-    
-      function direction_se() {
-        var bbox = getScreenBBox()
-        return {
-          top:  bbox.se.y,
-          left: bbox.e.x
-        }
-      }
-    
-      function initNode() {
-        var node = d3.select(document.createElement('div'))
-        node
-          .style('position', 'absolute')
-          .style('top', 0)
-          .style('opacity', 0)
-          .style('pointer-events', 'none')
-          .style('box-sizing', 'border-box')
-    
-        return node.node()
-      }
-    
-      function getSVGNode(el) {
-        el = el.node()
-        if(el.tagName.toLowerCase() === 'svg')
-          return el
-    
-        return el.ownerSVGElement
-      }
-    
-      function getNodeEl() {
-        if(node === null) {
-          node = initNode();
-          // re-add node to DOM
-          document.body.appendChild(node);
-        };
-        return d3.select(node);
-      }
-    
-      // Private - gets the screen coordinates of a shape
-      //
-      // Given a shape on the screen, will return an SVGPoint for the directions
-      // n(north), s(south), e(east), w(west), ne(northeast), se(southeast), nw(northwest),
-      // sw(southwest).
-      //
-      //    +-+-+
-      //    |   |
-      //    +   +
-      //    |   |
-      //    +-+-+
-      //
-      // Returns an Object {n, s, e, w, nw, sw, ne, se}
-      function getScreenBBox() {
-        var targetel   = target || d3.event.target;
-    
-        while ('undefined' === typeof targetel.getScreenCTM && 'undefined' === targetel.parentNode) {
-            targetel = targetel.parentNode;
-        }
-    
-        var bbox       = {},
-            matrix     = targetel.getScreenCTM(),
-            tbbox      = targetel.getBBox(),
-            width      = tbbox.width,
-            height     = tbbox.height,
-            x          = tbbox.x,
-            y          = tbbox.y
-    
-        point.x = x
-        point.y = y
-        bbox.nw = point.matrixTransform(matrix)
-        point.x += width
-        bbox.ne = point.matrixTransform(matrix)
-        point.y += height
-        bbox.se = point.matrixTransform(matrix)
-        point.x -= width
-        bbox.sw = point.matrixTransform(matrix)
-        point.y -= height / 2
-        bbox.w  = point.matrixTransform(matrix)
-        point.x += width
-        bbox.e = point.matrixTransform(matrix)
-        point.x -= width / 2
-        point.y -= height / 2
-        bbox.n = point.matrixTransform(matrix)
-        point.y += height
-        bbox.s = point.matrixTransform(matrix)
-    
-        return bbox
-      }
-    
-      return tip
-    };
-    
-})
-// D3-Tips
+    .classed("active", true)
+    .text("Percentage of Population Enrolled in Healthcare (%)");
+  });
