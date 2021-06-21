b3ee81ab82fe52e35dd54735e59caab574b5044f
Trumane12311 <Trumane12311@gmail.com>
2021-06-21 11:16:59 -0400

    Updated HTML colors, updated circles

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -1,7 +1,7 @@
 // @TODO: YOUR CODE HERE!
 // set the dimensions and margins of the graph
-let margin = {top: 10, right: 30, bottom: 50, left: 50},
-    width = 950 - margin.left - margin.right,
+let margin = {top: 10, right: 90, bottom: 50, left: 50},
+    width = 1200 - margin.left - margin.right,
     height = 775 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -11,10 +11,8 @@ const svg = d3.select("#scatter")
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-let chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+          "translate(" + margin.left + "," + margin.top + ")"
+          );
 
 //Read the data
 d3.csv("D3_data_journalism/data.csv").then(censusData => {
@@ -26,35 +24,40 @@ d3.csv("D3_data_journalism/data.csv").then(censusData => {
     console.log(data.income);
     data.healthcare = +data.healthcare;
     console.log(data.healthcare);
-    data.obesity = +data.obesity;
-    console.log(data.obesity);
-    data.poverty = +data.poverty;
-    console.log(data.poverty);
     data.abbr = data.abbr;
     console.log(data.abbr);
   })
 
   // Add X axis
   let x = d3.scaleLinear()
-    .domain([0, d3.max(censusData, d => d.income)])
+    .domain([35000, d3.max(censusData, d => d.income + 2000)])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
   
   let xlabel = svg.append("text")
-    .attr("x", 400)
-    .attr("y", 750)
+    .attr("x", width/2)
+    .attr("y", 760)
     .attr("value", "income") // value to grab for event listener
     .classed("active", true)
-    .text("Median Household Income ($)");
+    .text("Median Household Income ($)")
 
   // Add Y axis
   let y = d3.scaleLinear()
-    .domain([0, d3.max(censusData, d => d.healthcare)])
+    .domain([0, d3.max(censusData, d => d.healthcare + 2)])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
+
+  // Label the Y-Axis
+  svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left)
+  .attr("x", 50 - (height / 2))
+  .attr("dy", "1em")
+  .classed("active", true)
+  .text("Percentage of Population Lacking Healthcare Coverage (%)");
 
   let bottomAxis = d3.axisBottom(x)
   let leftAxis = d3.axisLeft(y)
@@ -70,22 +73,21 @@ d3.csv("D3_data_journalism/data.csv").then(censusData => {
     .style("border-radius", "5px")
     .style("padding", "10px")
 
-
-
   // A function that change this tooltip when the user hover a point.
   // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
   let mouseover = function(d) {
     tooltip
       .style("opacity", 1)
+      .on("click", mouseleave)
   }
 
   let mousemove = function(d) {
     tooltip
-      .html(`<strong>${d.state}</strong><hr><strong>Healthcare (%):</strong> ${d.healthcare} <br> 
+      .html(`<strong>${d.state}</strong><hr><strong>Without Healthcare:</strong> ${d.healthcare}% <br> 
       <strong>Household Income:</strong> ${d.income.toLocaleString('en-US', {
         style: 'currency',
-        currency: 'USD'
-      })}`)
+        currency: 'USD'}
+        )}`)
       .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
       .style("top", (d3.mouse(this)[1]) + "px")
   }
@@ -94,14 +96,14 @@ d3.csv("D3_data_journalism/data.csv").then(censusData => {
   let mouseleave = function(d) {
     tooltip
       .transition()
-      .duration(200)
+      .duration(4000)
       .style("opacity", 0)
   }
 
   // Add dots
   svg.append('g')
     .selectAll("dot")
-    .data(censusData.filter(function(d,i){return i})) // the .filter part is just to keep a few dots on the chart, not all of them
+    .data(censusData)
     .enter()
     .append("circle")
       .attr("cx", function (d) { return x(d.income); } )
@@ -114,8 +116,7 @@ d3.csv("D3_data_journalism/data.csv").then(censusData => {
     .on("mousemove", mousemove )
     .on("mouseleave", mouseleave )
 
-  let chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  let chartGroup = svg.append("g");
 
   chartGroup.append("text")
     .style("font-size", "10px")
@@ -123,23 +124,13 @@ d3.csv("D3_data_journalism/data.csv").then(censusData => {
     .data(censusData)
     .enter()
     .append("tspan")
-        .attr("x", function(data) {
-            return x(data.income);
+        .attr("x", function(d) {
+            return x(d.income)
         })
-        .attr("y", function(data) {
-            return y(data.healthcare);
+        .attr("y", function(d) {
+            return y(d.healthcare);
         })
-        .text(function(data) {
-            return data.abbr
+        .text(function(data) {   
+          return data.abbr
         });
-
-    
-    // Label the Y-Axis
-    svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x", 50 - (height / 2))
-    .attr("dy", "1em")
-    .classed("active", true)
-    .text("Percentage of Population Lacking Healthcare Coverage (%)");
-  });
+  })
